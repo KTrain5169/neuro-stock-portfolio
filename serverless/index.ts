c8340c96c914d0418a7b5ba0520da84645aa4981
KTrain5369 <me@git.ktrain5369.dev>
2025-12-19 21:53:19 +1000

    please

--- serverless/index.ts
+++ serverless/index.ts
@@ -176,8 +176,4 @@ app.post('/api/refresh', async (c) => {
     }
 });
 
-export default {
-    async fetch(request: Request, env: Env, ctx: ExecutionContext) {
-        return app.fetch(request, env, ctx);
-    }
-}
+export default app
